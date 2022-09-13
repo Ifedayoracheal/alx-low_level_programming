610bdf11aa4c4093ad95db99efb64d5f85a48dce
Ifedayoracheal <ifedayoracheal@gmail.com>
2022-09-12 20:56:22 -0700

    task 8

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of 2 digits.
+ * Numbers
+ * Return: 0
+ */
+int main(void)
+{
+    int i, p;
+    
+    
+    for (1 = 0; 1 < 100; i++)
+    {
+           
+        for (p = 0; p < 100; i++)
+                        { 
+
+               If (p > I)
+                   {
+                   putchar((i / 10) + '0');
+                   putchar((i % 10) + '0');
+                   putchar(' ');
+                                          putchar((p / 10) + '0')
+                                           Putchar((p % 10) + '0' );
+                          If (i  l= 98)
+            {
+                putchar(',');
+                putchar(' ');
+            }
+          }
+    
+                }
+    
+       }
+putchar('\n');
+return (0);
+}
+
+
