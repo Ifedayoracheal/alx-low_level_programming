6461211ff6426e2753e77195b8b0722fc74d55a7
Ifedayoracheal <ifedayoracheal@gmail.com>
2022-09-19 21:41:35 -0700

    Arrays are not pointers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,21 +9,6 @@
 */
 
 void print_array(int *a, int n)
-#include "main.h"
-
-/**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
-
-int main(void)
-{
-int i;
-
-i = 0;
-positive_or_negative(i);
-
-return (0);
 {
 	int inc;
 
